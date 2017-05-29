d85a3085901cfdd92751f0f632f48d92f137e6f5
Juan David Redondo <juandredondo@uniguajira.edu.co>
2017-05-29 16:41:50 -0500

    Cambio de  titulo pagina principal
    le cambie el nombre al index

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Proyecto de electiva de  profundizacion de base de datos!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
